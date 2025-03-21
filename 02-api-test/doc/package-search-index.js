a7bbb4e0297cc6e31d65c4f8466d23f3efe5cf6b
Kyaw Khant Zin <kyawkhantzin11@gmail.com>
2025-03-21 17:27:50 +0630

    Merge branch 'main' of git@github.com:KKhantZin/api_repo.git into main

--- 02-api-test/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jdc.anno3"}];updateSearchResults();
--- 02-api-test/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.jdc.anno3","l":"Documented1"}];updateSearchResults();
